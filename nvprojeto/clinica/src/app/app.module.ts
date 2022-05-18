13cd054c09cbbaac9518fb61648f9544c16df12f
Ja-Yuna <chaozinhaa@gmail.com>
2022-05-18 13:06:17 -0300

    Subindo mudanças no template e telas

---
+++ nvprojeto/clinica/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroContatoComponent } from './cadastro-contato/cadastro-contato.component';
+import { ConsultaContatoComponent } from './consulta-contato/consulta-contato.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroContatoComponent,
+    ConsultaContatoComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
